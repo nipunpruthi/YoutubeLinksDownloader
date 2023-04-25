955a440088f8ba91cb750935fe21add6bd495bfa
Nipun Pruthi <nipun.pruthi@BLRETV-QGPNLVCF.local>
2023-04-25 17:09:11 +0530

    facebook support

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ N=7
 while read -r name url; do
     ((i=i%N));((i++==0)) && wait
     echo "url : ${url} started, file name : ${name}" ;
-    youtube-dl ${url} --extract-audio --audio-format "mp3" --audio-quality 2 -o "${name}%(ext)s" &
+    youtube-dl ${url} --extract-audio --audio-format "mp3" --audio-quality 3 -o "${name}%(ext)s" &
 echo "url : ${url} completed, file name : ${name}" ;
 
 done < arguments.txt
