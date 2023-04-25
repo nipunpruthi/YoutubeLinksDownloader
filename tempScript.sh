7d76865cf6719cc8fd25e59a19f18beefb5f9213
Nipun Pruthi <nipun.pruthi@BLRETV-QGPNLVCF.local>
2023-04-25 17:28:57 +0530

    facebook support

--- tempScript.sh
+++ tempScript.sh
@@ -2,9 +2,7 @@
 
 N=7
 while read -r name url; do
-    ((i=i%N));((i++==0)) && wait
     echo "url : ${url} started, file name : ${name}" ;
-    youtube-dl ${url} --extract-audio --audio-format "mp3" --audio-quality 3 -o "${name}%(ext)s" &
-echo "url : ${url} completed, file name : ${name}" ;
-
-done < temp.txt
+    youtube-dl ${url} --extract-audio --audio-format "mp3" --audio-quality 3 -o "${name}%(ext)s"
+    echo "url : ${url} completed, file name : ${name}" ;
+done < arguments.txt
